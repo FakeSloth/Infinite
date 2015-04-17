4d43f38a3dee655239e706a72887b44d1ccbd623
Phil <birkal@outlook.com>
2015-04-17 15:57:16 -0700

    Add give and take in economy

--- infinite/economy.js
+++ infinite/economy.js
@@ -18,5 +18,59 @@ module.exports = {
             if (err) return deferred.reject(new Error(err));
         });
         return deferred.promise;
+    },
+
+    /**
+     * Give a user a ceratin amount of money.
+     *
+     * @param {String} name
+     * @param {Number} amount
+     * @return {Promise}
+     */
+    give: function(name, amount) {
+        var deferred = Q.defer();
+        User.findOne({name: toId(name)})
+        .then(function(user) {
+            if (!user) {
+                user = new userModel({
+                    name: toId(name),
+                    money: amount
+                });
+                return user.save(function(err) {
+                    if (err) return deferred.reject(new Error(err));
+                    deferred.resolve(user.money);
+                });
+            }
+            user.money += amount;
+            user.save(function(err) {
+                if (err) return deferred.reject(new Error(err));
+                deferred.resolve(user.money);
+            });
+        }, function(err) {
+            if (err) return deferred.reject(new Error(err));
+        });
+        return deferred.promise;
+    },
+
+    /**
+     * Take a certain amount of money from a user.
+     * 
+     * @param {String} name
+     * @param {Number} amount
+     * @return {Promise}
+     */
+    take: function(name, amount) {
+        var deferred = Q.defer();
+        User.findOne({name: toId(name)})
+        .then(function(user) {
+            user.money -= amount;
+            user.save(function(err) {
+                if (err) return deferred.reject(new Error(err));
+                deferred.resolve(user.money);
+            })
+        }, function(err) {
+            if (err) return deferred.reject(new Error(err));
+        });
+        return deferred.promise;
     }
 };
