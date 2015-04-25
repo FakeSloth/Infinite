c7239471c5609df19644d97312701f4fb6b63914
Phil <birkal@outlook.com>
2015-04-24 22:34:04 -0700

    Remove roomfounder and add room founder to roomhelp

--- infinite/commands/roomrank.js
+++ infinite/commands/roomrank.js
@@ -39,27 +39,6 @@ module.exports = {
         Rooms.global.writeChatRoomData();
     }, 
 
-    roomleader: function (target, room, user) {
-        if (!room.chatRoomData) {
-            return this.sendReply("/roomleader - This room isn't designed for per-room moderation to be added");
-        }
-        target = this.splitTarget(target, true);
-        var targetUser = this.targetUser;
-
-        if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
-
-        if (room.founder !== user.userid && !this.can('makeroom')) return this.sendReply('/roomleader - Access denied.');
-
-        if (!room.auth) room.auth = room.chatRoomData.auth = {};
-
-        var name = targetUser.name;
-
-        room.auth[targetUser.userid] = '&';
-        this.addModCommand(name + " was appointed Room Leader by " + user.name + ".");
-        room.onUpdateIdentity(targetUser);
-        Rooms.global.writeChatRoomData();
-    },
-
     deroomowner: function (target, room, user) {
         if (!room.auth) {
             return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
@@ -79,5 +58,43 @@ module.exports = {
         if (room.chatRoomData) {
             Rooms.global.writeChatRoomData();
         }
-    }
+    },
+
+    roomhelp: function (target, room, user) {
+        if (room.id === 'lobby' || room.battle) return this.sendReply("This command is too spammy for lobby/battles.");
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox(
+            "Room drivers (%) can use:<br />" +
+            "- /warn OR /k <em>username</em>: warn a user and show the Pokemon Showdown rules<br />" +
+            "- /mute OR /m <em>username</em>: 7 minute mute<br />" +
+            "- /hourmute OR /hm <em>username</em>: 60 minute mute<br />" +
+            "- /unmute <em>username</em>: unmute<br />" +
+            "- /announce OR /wall <em>message</em>: make an announcement<br />" +
+            "- /modlog <em>username</em>: search the moderator log of the room<br />" +
+            "- /modnote <em>note</em>: adds a moderator note that can be read through modlog<br />" +
+            "<br />" +
+            "Room moderators (@) can also use:<br />" +
+            "- /roomban OR /rb <em>username</em>: bans user from the room<br />" +
+            "- /roomunban <em>username</em>: unbans user from the room<br />" +
+            "- /roomvoice <em>username</em>: appoint a room voice<br />" +
+            "- /roomdevoice <em>username</em>: remove a room voice<br />" +
+            "- /modchat <em>[off/autoconfirmed/+]</em>: set modchat level<br />" +
+            "<br />" +
+            "Room owners (#) can also use:<br />" +
+            "- /roomintro <em>intro</em>: sets the room introduction that will be displayed for all users joining the room<br />" +
+            "- /rules <em>rules link</em>: set the room rules link seen when using /rules<br />" +
+            "- /roommod, /roomdriver <em>username</em>: appoint a room moderator/driver<br />" +
+            "- /roomdemod, /roomdedriver <em>username</em>: remove a room moderator/driver<br />" +
+            "- /modchat <em>[%/@/#]</em>: set modchat level<br />" +
+            "- /declare <em>message</em>: make a large blue declaration to the room<br />" +
+            "- !htmlbox <em>HTML code</em>: broadcasts a box of HTML code to the room<br />" +
+            "- !showimage <em>[url], [width], [height]</em>: shows an image to the room<br />" +
+            "<br />" +
+            "Room founders (#) can also use<br />" +
+            "- /roomowner <em>username</em> - Appoints username as a room owner<br />" +
+            "<br />" +
+            "More detailed help can be found in the <a href=\"https://www.smogon.com/sim/roomauth_guide\">roomauth guide</a><br />" +
+            "</div>"
+        );
+    },
 };
