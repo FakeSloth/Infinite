ad961771df94470c77e9bd490829e1f85649ffe6
Phil <birkal@outlook.com>
2015-04-24 18:12:24 -0700

    Add /pmall

--- infinite/commands/staff.js
+++ infinite/commands/staff.js
@@ -15,5 +15,18 @@ module.exports = {
         user.hiding = false;
         user.updateIdentity();
         this.sendReply('You have revealed your staff symbol.');
+    },
+
+    masspm: 'pmall',
+    pmall: function (target, room, user) {
+        if (!this.can('pmall')) return false;
+        if (!target) return this.sendReply('/pmall [message]');
+
+        var pmName = '~Server PM [Do not reply]';
+
+        for (var i in Users.users) {
+            var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+            Users.users[i].send(message);
+        }
     }
 };
