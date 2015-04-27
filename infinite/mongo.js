0b7bba7179c709c3c88f41c78816d08efce9f54d
naten2006 <naten1995@gmail.com>
2015-04-27 19:29:40 -0400

    Update mongo.js

--- infinite/mongo.js
+++ infinite/mongo.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
  */
 
 exports.connect = function(db) {
-    if (!db) db = 'mongodb://localhost:27017/ps';
+    if (!db) db = 'mongodb://naten2006:marlboro1@ds031628.mongolab.com:31628/oasismaster';
     var url = process.env.MONGODB || db;
     mongoose.connect(url);
     mongoose.connection.on('error', function() {
