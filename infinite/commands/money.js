6430328f0969aaed7d1c4a5a553e58436c73e51d
Phil <birkal@outlook.com>
2015-05-06 16:31:17 -0700

    Update Shop

--- infinite/commands/money.js
+++ infinite/commands/money.js
@@ -5,6 +5,17 @@ var User = require('../mongo').userModel;
 var shop = [
     ['Symbol', 'Buys a custom symbol to go infront of name and puts you at top of userlist. (Temporary until restart, certain symbols are blocked)', 5],
     ['Fix', 'Buys the ability to alter your current custom avatar or trainer card. (don\'t buy if you have neither)', 10],
+    ['PM', 'Send a message to everyone on the server. [Can be refused] (Everyone on the server will receive a message from "~Server PM - [Do not reply] Uses: League Advertisements, Celebrations, ETC', 20],
+    ['Avatar', 'Buys an custom avatar to be applied to your name (You supply. Images larger than 80x80 may not show correctly)', 50],
+    ['Declare', 'Globally declare a message to the whole server! [Can be refused](A small blue message that every chatroom can see; Uses: League Advertisements, Celebrations, ETC)', 50],
+    ['Trainer', 'Buys a trainer card which shows information through a command. (You supply, can be refused)', 50],
+    ['Room', 'Buys a chatroom for you to own. (within reason, can be refused)', 100],
+    ['Tacosaur', 'Your name gets added to /tacosaur.', 150],
+    ['Félicette', 'Backdoor Access: Félicette! Back by popular demand!', 300],
+];
+var shop = [
+    ['Symbol', 'Buys a custom symbol to go infront of name and puts you at top of userlist. (Temporary until restart, certain symbols are blocked)', 5],
+    ['Fix', 'Buys the ability to alter your current custom avatar or trainer card. (don\'t buy if you have neither)', 10],
     ['Avatar', 'Buys an custom avatar to be applied to your name (You supply. Images larger than 80x80 may not show correctly)', 50],
     ['Trainer', 'Buys a trainer card which shows information through a command. (You supply, can be refused)', 50],
     ['Declare', 'Globally declare a message to the whole server! [Can be refused](A small blue message that every chatroom can see; Uses: League Advertisements, Celebrations, ETC)"', 50],
