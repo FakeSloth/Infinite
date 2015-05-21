f9420059e37e2dff134e74cb0da8c6c28cdf8742
Phil <birkal@outlook.com>
2015-05-20 23:15:17 -0700

    Use override crashlogger

--- infinite/commands/cards.js
+++ infinite/commands/cards.js
@@ -391,9 +391,9 @@ module.exports = {
                     });
                     // avoid spam in lobby
                     if (room.id === 'lobby' && price < 15) {
-                        room.addRaw('<button name="send" value="/buyitem ' + cardInstance.id + '"><b>' + user.name + '</b> is selling <b><font color="' + colors[toTitleCase(cardInstance.rarity)] + '">' + toTitleCase(cardInstance.rarity) + '</font> ' + toTitleCase(cardInstance.name) + '</b> for <b><font color="' + accent + '">' + price + Economy.currency(price) + '</font><b></button>');
-                    } else {
                         self.sendReply('You are selling this card for ' + price + Economy.currency(price) + '.');
+                    } else {
+                        room.addRaw('<button name="send" value="/buyitem ' + cardInstance.id + '"><b>' + user.name + '</b> is selling <b><font color="' + colors[toTitleCase(cardInstance.rarity)] + '">' + toTitleCase(cardInstance.rarity) + '</font> ' + toTitleCase(cardInstance.name) + '</b> for <b><font color="' + accent + '">' + price + Economy.currency(price) + '</font><b></button>');
                     }
                     room.update();
                     cardInstance.cid = cardInstance.id;
@@ -444,9 +444,9 @@ module.exports = {
                     addCard(user.userid, card);
                     // avoid spam in lobby
                     if (room.id === 'lobby' && item.price < 25) {
-                        room.addRaw('<b>' + user.name + '</b> has bought <button name="send" value="/card ' + card.id + ', ' + user.userid + '"<b><font color="' + colors[toTitleCase(card.rarity)] + '">' + toTitleCase(card.rarity) + '</font> ' + toTitleCase(card.name) + '</b></button> for <b><font color="' + accent + '">' + item.price + Economy.currency(item.price) + '</font><b>.');
-                    } else {
                         self.sendReply('You bought this card successfully.');
+                    } else {
+                        room.addRaw('<b>' + user.name + '</b> has bought <button name="send" value="/card ' + card.id + ', ' + user.userid + '"<b><font color="' + colors[toTitleCase(card.rarity)] + '">' + toTitleCase(card.rarity) + '</font> ' + toTitleCase(card.name) + '</b></button> for <b><font color="' + accent + '">' + item.price + Economy.currency(item.price) + '</font><b>.');
                     }
                     room.update();
                 });
